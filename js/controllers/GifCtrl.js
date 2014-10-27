8f0e7f90c6f1c13584c40417875b1b298d167d33
Adam Scott <adam.scott@cfpb.gov>
2014-10-27 15:20:58 -0400

    pull gifs from json file

--- js/controllers/GifCtrl.js
+++ js/controllers/GifCtrl.js
@@ -5,27 +5,14 @@ exports.inject = function(app) {
   return exports.controller;
 };
 
-exports.controller = function($scope) {
-  //$scope.gifMe = false;
+exports.controller = function($scope, $http) {
+  $http.get('../images.json').success(function(data) {
+    $scope.gifs = data.data['images'];
 
-  $scope.gifs = [
-    'https://i.imgur.com/66hHres.gif',
-    'https://i.imgur.com/ThFlM3A.gif',
-    'https://i.imgur.com/WZ1HnE7.gif',
-    'https://i.imgur.com/GvfzqGE.gif',
-    'https://i.imgur.com/EuCM6t9.gif',
-    'https://i.imgur.com/Q5HTevU.gif',
-    'https://i.imgur.com/C2MPrfh.gif',
-    'https://i.imgur.com/CkRseuL.gif',
-    'https://i.imgur.com/A4yxphQ.gif',
-    'https://i.imgur.com/1GlHZbX.gif',
-    'https://i.imgur.com/y4QDCGR.gif'
-  ];
+    $scope.randomGif = function() {
+      $scope.gif = $scope.gifs[Math.floor(Math.random() * $scope.gifs.length)].link;
+    };
 
-  $scope.gif = $scope.gifs[Math.floor(Math.random() * $scope.gifs.length)];
-
-  $scope.randomGif = function() {
-    $scope.gif = $scope.gifs[Math.floor(Math.random() * $scope.gifs.length)];
-  };
-
+    $scope.randomGif();
+  });
 };
